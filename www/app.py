bbefefb1af9c84d87046798828c6c4f100fcd2e2
Haley Zhu <zhuhe212@163.com>
2018-08-01 13:30:06 +0800

    app ready

---
+++ www/app.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, host='127.0.0.1', port='8000')
